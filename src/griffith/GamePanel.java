19bada9925253b9201a22390f97c8bf8fa78adfe
Nika2866 <nikahrytsyk1@gmail.com>
2025-03-26 14:44:29 +0000

    Initialized 'xDelta' and 'yDelta' for dynamic positioning of the rectangle; updated rectangle's position based on these deltas.

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -11,6 +11,8 @@ import inputs.MouseInputs;
 public class GamePanel extends JPanel { // Extends JPanel to create custom panel
 
 	private MouseInputs mouseInputs; // Declares a private variable 'mouseInputs' of type MouseInputs, which will be used to handle mouse-related input events.
+	private int xDelta = 0, yDelta = 0; // Declares and initializes two integer variables 'xDelta' and 'yDelta' to track the change in position (delta) for X and Y coordinates.
+	
 	public GamePanel() {
 		// Constructor for GamePanel, no initialization needed
 		
@@ -25,7 +27,7 @@ public class GamePanel extends JPanel { // Extends JPanel to create custom panel
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g); // Call superclass method to handle standard painting
-		g.fillRect(100, 100, 200, 50); // Draw a rectangle on the panel at position (100, 100) with width 200 and
-										// height 50
+		g.fillRect(100 + xDelta, 100 + yDelta, 200, 50); // Draw a rectangle on the panel at position (100, 100) with width 200 and
+														// height 50
 	}
 }
