3eede097e3857e6ee5d8101a2ed84a0cc07bd7a6
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-03-26 10:18:55 +0000

    Add initial GameWindow class for creating a basic GUI window

--- src/griffith/GameWindow.java
+++ src/griffith/GameWindow.java
@@ -1,5 +1,13 @@
 package griffith;
 
-public class GameWindow {
+import javax.swing.JFrame; // Import the JFrame class for GUI creation
 
+public class GameWindow {
+	private JFrame jframe; // Declare a JFrame object
+
+	public GameWindow() {
+		jframe = new JFrame(); // Initialize the JFrame object
+		jframe.setSize(400, 400); // Set the size of the JFrame
+		jframe.setVisible(true); // Make the JFrame visible
+	}
 }
