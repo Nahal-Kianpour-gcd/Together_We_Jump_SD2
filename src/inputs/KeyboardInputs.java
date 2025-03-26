25e413754894234e05f9970c8242760928c0e51a
Nika2866 <nikahrytsyk1@gmail.com>
2025-03-26 13:06:47 +0000

    Added KeyboardInputs class implementing KeyListener

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,5 +1,26 @@
 package inputs;
 
-public class KeyboardInputs {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+	// Class to handle keyboard input events using the KeyListener interface
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
