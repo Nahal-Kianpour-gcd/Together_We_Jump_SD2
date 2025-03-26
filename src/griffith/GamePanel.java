f12768611206a6eaa90951e15a94d62b51e87372
Nika2866 <nikahrytsyk1@gmail.com>
2025-03-26 14:37:29 +0000

    Added methods to make Mouse work

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -6,12 +6,19 @@ import java.awt.Graphics; // Import Graphics class for drawing capabilities
 import javax.swing.JPanel; // Import JPanel to create a panel
 
 import inputs.KeyboardInputs;
+import inputs.MouseInputs;
 
 public class GamePanel extends JPanel { // Extends JPanel to create custom panel
 
+	private MouseInputs mouseInputs; // Declares a private variable 'mouseInputs' of type MouseInputs, which will be used to handle mouse-related input events.
 	public GamePanel() {
 		// Constructor for GamePanel, no initialization needed
+		
+		mouseInputs = new  MouseInputs();
 		addKeyListener(new KeyboardInputs()); 
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+		
 		
 	}
 
