4e55ef01de0666b55a0df995ba6ae987e8e4e062
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-03-26 10:49:42 +0000

    Initialize GameWindow and GamePanel in Game class

--- src/griffith/Game.java
+++ src/griffith/Game.java
@@ -1,9 +1,11 @@
-package griffith; // Package declaration
+package griffith;
 
 public class Game {
 	private GameWindow gameWindow; // Declare a GameWindow object
+	private GamePanel gamePanel; // Declare a GamePanel object
 
 	public Game() {
-		gameWindow = new GameWindow(); // Initialize the GameWindow object in the constructor
+		gameWindow = new GameWindow(); // Initialize the GameWindow object
+		gamePanel = new GamePanel(); // Initialize the GamePanel object
 	}
 }
