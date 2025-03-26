39e88feed37d4189483b48927d34cc2a33d29284
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-03-26 10:24:56 +0000

    Add Game class to initialize GameWindow object

--- src/griffith/Game.java
+++ src/griffith/Game.java
@@ -1,5 +1,9 @@
-package griffith;
+package griffith; // Package declaration
 
 public class Game {
+	private GameWindow gameWindow; // Declare a GameWindow object
 
+	public Game() {
+		gameWindow = new GameWindow(); // Initialize the GameWindow object in the constructor
+	}
 }
