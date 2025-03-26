1b4b2d1bb13a2e15c2340a430dcf4890c9197deb
Nika2866 <nikahrytsyk1@gmail.com>
2025-03-26 12:56:39 +0000

    Added KeyListener for keyboard input handling (methods to be implemented)

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -1,12 +1,39 @@
 package griffith;
 
 import java.awt.Graphics; // Import Graphics class for drawing capabilities
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel; // Import JPanel to create a panel
 
 public class GamePanel extends JPanel { // Extends JPanel to create custom panel
 
 	public GamePanel() {
 		// Constructor for GamePanel, no initialization needed
+		addKeyListener(new KeyListener() { 
+			// Adding a KeyListener to handle keyboard input events
+			// Methods will be implemented to manage keyTyped, keyPressed, and keyReleased actions
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
 	}
 
 	@Override
