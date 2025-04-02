e35bfca32f785ea99d49c08207c0d1df3d924fd0
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-02 13:42:38 +0100

    Disabled window resizing to maintain consistent layout and appearance.

--- src/griffith/GameWindow.java
+++ src/griffith/GameWindow.java
@@ -10,7 +10,8 @@ public class GameWindow {
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set default close operation to exit the application
 		jframe.add(gamePanel); // Add the GamePanel to the JFrame
 		jframe.setLocationRelativeTo(null); // Centers the JFrame in the middle of the screen
-		jframe.pack(); //fit the size of the window to its size of its components
+		jframe.setResizable(false); // Prevents the window from being resized by the user
+		jframe.pack(); // fit the size of the window to its size of its components
 		jframe.setVisible(true); // Make the JFrame visible
 	}
 }
