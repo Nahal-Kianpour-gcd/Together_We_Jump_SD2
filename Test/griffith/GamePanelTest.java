f1fff1a1670491be39c508bbdabb336e77affcd1
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-03 18:19:36 +0100

    Add test for Player 1 moving left

--- Test/griffith/GamePanelTest.java
+++ Test/griffith/GamePanelTest.java
@@ -15,5 +15,20 @@ class GamePanelTest {
 		gamePanel = new GamePanel();
 		keyboardInputs = new KeyboardInputs(gamePanel);
 	}
+	
+    @Test
+    public void testPlayer1MovesLeft() {
+        float initialX = gamePanel.getXDelta1();
+        keyboardInputs.keyPressed(new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_A, 'A'));
+
+        // Simulate multiple updates
+        for (int i = 0; i < 5; i++) {
+            gamePanel.update();
+        }
+
+        keyboardInputs.keyReleased(new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, KeyEvent.VK_A, 'A'));
+
+        assertEquals(initialX - 15, gamePanel.getXDelta1(), "Player 1 should move left by 15 units after multiple updates.");
+    }
 
 }
