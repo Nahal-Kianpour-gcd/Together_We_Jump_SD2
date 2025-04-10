9689f6621392357ddbeb8bfe5c7337b1dc2fe9b1
Polina Shtefan <polina.shtefan@student.griffith.ie>
2025-04-10 14:07:48 +0100

    Add getSpriteAmount() method (Constants.java) for sprite frame management.

--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -20,4 +20,16 @@ public class Constants {
     //Animation frame counts
     public static final int IDLE_FRAMES = 11; //Number of frames for idle animation
     public static final int RUN_FRAMES = 12;  //Number of frames for run animation
+    
+    //Method to get the number of sprite frames based on action type
+    public static int getSpriteAmount(int action) {
+        switch (action) {
+            case PlayerConstants.IDLE:
+                return IDLE_FRAMES;
+            case PlayerConstants.RUNNING:
+                return RUN_FRAMES;
+            default:
+                return 0; //Return 0 for unknown action
+        }
+    }
 }
