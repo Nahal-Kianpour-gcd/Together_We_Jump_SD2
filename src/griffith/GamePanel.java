1e94a3e5e46c349dacf8eb10a85dbc402f460a4f
Thanh Phuong Hoang <thanhphuonghoang@student.griffith.ie>
2025-04-15 14:51:18 +0100

    fixed error's about local variables

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -10,83 +10,80 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
-
 import javax.swing.Timer;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 import utilz.Constants;
+import entities.Player;
 
-public class GamePanel extends JPanel { // Extends JPanel to allow custom drawing and event handling
+public class GamePanel extends JPanel {
 
-	private MouseInputs mouseInputs; // Handles mouse input events (clicks, movement)
-
+	private MouseInputs mouseInputs;
 	private KeyboardInputs keyboardInputs;
 	
 	private Player player1;
 	private Player player2;
 
-
-	
+	/* Old code - Start */
 	/*
 	// First player position (WASD)
 	private float xDelta = 100, yDelta = 100;
 	// Second player position (Arrow keys)
 	private float xDelta2 = 200, yDelta2 = 100;
 
-	//private BufferedImage player1img; // Holds the loaded sprite image
-	//private BufferedImage player2Img; // Image for second player
+	private static final int IDLE_FRAMES = Constants.IDLE_FRAMES;
+	private static final int RUN_FRAMES = Constants.RUN_FRAMES;
 
-	private static final int IDLE_FRAMES = Constants.IDLE_FRAMES; // Use constant from Constants class |PS
-	private static final int RUN_FRAMES = Constants.RUN_FRAMES; // Use constant from Constants class |PS
-
-	private BufferedImage[] idleAnimation1; // Player 1 idle animation frames |PS
-	private BufferedImage[] runAnimation1; // Player 1 run animation frames |PS
-	private BufferedImage[] idleAnimation2; // Player 2 idle animation frames |PS
-	private BufferedImage[] runAnimation2; // Player 2 run animation frames |PS
+	private BufferedImage[] idleAnimation1;
+	private BufferedImage[] runAnimation1;
+	private BufferedImage[] idleAnimation2;
+	private BufferedImage[] runAnimation2;
 
 	//Player states
-	private int playerDir1 = -1, playerDir2 = -1; // Separate directions for each player
-	private boolean moving1 = false, moving2 = false; // Separate movement states for each player
+	private int playerDir1 = -1, playerDir2 = -1;
+	private boolean moving1 = false, moving2 = false;
 
-	private int aniTick1, aniTick2, aniSpeed = 15; // PS
-	private int aniIndex1 = 0, aniIndex2 = 0; // PS
-	private int playerAction1 = 0, playerAction2 = 0; // PS
+	private int aniTick1, aniTick2, aniSpeed = 15;
+	private int aniIndex1 = 0, aniIndex2 = 0;
+	private int playerAction1 = 0, playerAction2 = 0;
 	*/
-	public GamePanel() {
+	/* Old code - End */
 
-		//Add input listeners for keyboard and mouse
+	public GamePanel() {
 		mouseInputs = new MouseInputs(this);
 		keyboardInputs = new KeyboardInputs(this);
+
+		/* Old initialization code - Start */
 		/*
-		//importImg();
 		loadAnimations();
 		*/
+		/* Old initialization code - End */
+
 		initializePlayers();
 		setPanelSize();
+		
 		addKeyListener(keyboardInputs);
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 
-		//Create and start game loop
-		Timer gameLoop = new Timer(16, new ActionListener() { // 60 FPS
+		Timer gameLoop = new Timer(16, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				/* update(); */ // Renamed to updateGame() to avoid conflict with Java's built-in AWT update() method
-				updateGame();   // Calls custom game logic update method |NK
+				updateGame();
 				repaint();
 			}
 		});
 		gameLoop.start();
 	}
-	
+
 	private void initializePlayers() {
 		player1 = new Player(100, 100, "/image-resources/Main_Characters/Ninja_Frog");
 		player2 = new Player(200, 100, "/image-resources/Main_Characters/Virtual_Guy");
 	}
 
+	/* Old animation methods - Start */
 	/*
-	//Method to load animation assets.|PS
 	private void loadAnimations() {
 		idleAnimation1 = new BufferedImage[IDLE_FRAMES];
 		runAnimation1 = new BufferedImage[RUN_FRAMES];
@@ -99,7 +96,6 @@ public class GamePanel extends JPanel { // Extends JPanel to allow custom drawin
 		loadAnimation("/image-resources/Main_Characters/Virtual_Guy/Run (32x32).png", runAnimation2);
 	}
 
-	//Method to read the sprite sheet and extract individual frames. |PS
 	private void loadAnimation(String path, BufferedImage[] animation) {
 		try {
 			InputStream is = getClass().getResourceAsStream(path);
@@ -107,11 +103,11 @@ public class GamePanel extends JPanel { // Extends JPanel to allow custom drawin
 				throw new RuntimeException("Resource not found: " + path);
 			}
 			BufferedImage img = ImageIO.read(is);
-			int frameWidth = img.getWidth() / animation.length; // Calculate frame width
-			int frameHeight = img.getHeight(); // Height remains the same
+			int frameWidth = img.getWidth() / animation.length;
+			int frameHeight = img.getHeight();
 
 			for (int i = 0; i < animation.length; i++) {
-				animation[i] = img.getSubimage(i * frameWidth, 0, frameWidth, frameHeight); // Extract each frame
+				animation[i] = img.getSubimage(i * frameWidth, 0, frameWidth, frameHeight);
 			}
 			is.close();
 		} catch (IOException e) {
@@ -119,29 +115,97 @@ public class GamePanel extends JPanel { // Extends JPanel to allow custom drawin
 		}
 	}
 
-	// Loads a sprite image from the resources folder
-/*	private void importImg() {
-		// Load the image as a stream from the class-path.
-		InputStream is = getClass().getResourceAsStream("/image-resources/Main_Characters/Ninja_Frog/Idle (32x32).png");
-		try {
-			// Read the image from the input stream and assign it to img
-			player1img = ImageIO.read(is);
-		} catch (IOException e) {
-			e.printStackTrace(); // Print error details if the image fails to load
+	private void setAnimation() {
+		//Set Player 1 animation state
+		if (moving1) {
+			playerAction1 = 1; //RUN
+		} else {
+			playerAction1 = 0; //IDLE
+		}
+		
+		//Set Player 2 animation state
+		if (moving2) {
+			playerAction2 = 1; //RUN
+		} else {
+			playerAction2 = 0; //IDLE
+		}
+	}
+
+	private void updatePos() {
+		// Update first player position
+		if (moving1) {
+			switch (playerDir1) {
+				case 0: // LEFT
+					xDelta -= 3;
+					break;
+				case 1: // UP
+					yDelta -= 3;
+					break;
+				case 2: // RIGHT
+					xDelta += 3;
+					break;
+				case 3: // DOWN
+					yDelta += 3;
+					break;
+			}
 		}
 
-		InputStream is2 = getClass()
-				.getResourceAsStream("/image-resources/Main_Characters/Virtual_Guy/Idle (32x32).png"); // or whatever
-																										// sprite you
-																										// use
-		try {
-
-			player2Img = ImageIO.read(is2);
-		} catch (IOException e) {
-			e.printStackTrace();
+		// Update second player position
+		if (moving2) {
+			switch (playerDir2) {
+				case 0: // LEFT
+					xDelta2 -= 3;
+					break;
+				case 1: // UP
+					yDelta2 -= 3;
+					break;
+				case 2: // RIGHT
+					xDelta2 += 3;
+					break;
+				case 3: // DOWN
+					yDelta2 += 3;
+					break;
+			}
 		}
-	} 
-	*/
+	}
+// Method to switch between animations. |PS
+	private void updateAnimationTick() {
+		// Update first player animation
+		aniTick1++;
+		if (aniTick1 >= aniSpeed) {
+			aniTick1 = 0;
+			int maxFrames;
+			if (playerAction1 == 0) {
+				maxFrames = IDLE_FRAMES;
+			} else {
+				maxFrames = RUN_FRAMES;
+			}
+			if (aniIndex1 < maxFrames - 1) {
+				aniIndex1++;
+			} else {
+				aniIndex1 = 0;
+			}
+		}
+
+		// Update second player animation
+		aniTick2++;
+		if (aniTick2 >= aniSpeed) {
+			aniTick2 = 0;
+			int maxFrames;
+			if (playerAction2 == 0) {
+				maxFrames = IDLE_FRAMES;
+			} else {
+				maxFrames = RUN_FRAMES;
+			}
+			if (aniIndex2 < maxFrames - 1) {
+				aniIndex2++;
+			} else {
+				aniIndex2 = 0;
+			}
+		}
+	}
+	
+	/* Old animation methods - End */
 
 	private void setPanelSize() {
 		Dimension size = new Dimension(1280, 800);
@@ -194,34 +258,6 @@ public class GamePanel extends JPanel { // Extends JPanel to allow custom drawin
 		}
 	}
 
-	
-	//Method to set the animation state. |PS
-	private void setAnimation() {
-	    //Set Player 1 animation state
-	    if (moving1) {
-	        playerAction1 = 1; //RUN
-	    } else {
-	        playerAction1 = 0; //IDLE
-	    }
-	    
-	    //Set Player 2 animation state
-	    if (moving2) {
-	        playerAction2 = 1; //RUN
-	    } else {
-	        playerAction2 = 0; //IDLE
-	    }
-	}
-
-	/*public void update() {
-		keyboardInputs.update();
-		updateAnimationTick();
-		setAnimation(); 
-		updatePos();
-	}*/
-	
-	 // Renamed from update() to updateGame() to avoid conflicts with Java's built-in update(Graphics g)
-	 // method in AWT/Swing. Helps prevent confusion with the painting system and keeps game logic separate. |NK
-	 //
 	public void updateGame() {
 		keyboardInputs.update();
 		/* Old update code - Start */
@@ -233,46 +269,6 @@ public class GamePanel extends JPanel { // Extends JPanel to allow custom drawin
 		/* Old update code - End */
 		player1.update();
 		player2.update();
-	}
-
-
-
-	private void updatePos() {
-		// Update first player position
-		if (moving1) {
-			switch (playerDir1) {
-				case 0: // LEFT
-					xDelta -= 3;
-					break;
-				case 1: // UP
-					yDelta -= 3;
-					break;
-				case 2: // RIGHT
-					xDelta += 3;
-					break;
-				case 3: // DOWN
-					yDelta += 3;
-					break;
-			}
-		}
-
-		// Update second player position
-		if (moving2) {
-			switch (playerDir2) {
-				case 0: // LEFT
-					xDelta2 -= 3;
-					break;
-				case 1: // UP
-					yDelta2 -= 3;
-					break;
-				case 2: // RIGHT
-					xDelta2 += 3;
-					break;
-				case 3: // DOWN
-					yDelta2 += 3;
-					break;
-			}
-		}
 	}
 
 	@Override
@@ -299,55 +295,15 @@ public class GamePanel extends JPanel { // Extends JPanel to allow custom drawin
 		player2.render(g);
 	}
 	// Returns Player 1's current X position
-		public float getXDelta1() {
-			return player1.getX();
-		}
-		// Returns Player 2's current X position
-		public float getXDelta2() {
-			return player2.getX();
-		}
-		// Returns Player 2's Y position as an Integer (rounded from float)
-		public int getYDelta2() {
-			return (int) player2.getY();
-		}
-
-//Method to switch between animations. |PS
-	private void updateAnimationTick() {
-		// Update first player animation
-		aniTick1++;
-		if (aniTick1 >= aniSpeed) {
-			aniTick1 = 0;
-			int maxFrames;
-			if (playerAction1 == 0) {
-				maxFrames = IDLE_FRAMES;
-			} else {
-				maxFrames = RUN_FRAMES;
-			}
-			if (aniIndex1 < maxFrames - 1) {
-				aniIndex1++;
-			} else {
-				aniIndex1 = 0;
-			}
-		}
-
-		// Update second player animation
-		aniTick2++;
-		if (aniTick2 >= aniSpeed) {
-			aniTick2 = 0;
-			int maxFrames;
-			if (playerAction2 == 0) {
-				maxFrames = IDLE_FRAMES;
-			} else {
-				maxFrames = RUN_FRAMES;
-			}
-			if (aniIndex2 < maxFrames - 1) {
-				aniIndex2++;
-			} else {
-				aniIndex2 = 0;
-			}
-		}
+	public float getXDelta1() {
+		return player1.getX();
 	}
-	
-	
-
-
+	// Returns Player 2's current X position
+	public float getXDelta2() {
+		return player2.getX();
+	}
+	// Returns Player 2's Y position as an Integer (rounded from float)
+	public int getYDelta2() {
+		return (int) player2.getY();
+	}
+}
