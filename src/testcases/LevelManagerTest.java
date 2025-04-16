024f379629fff42c8d8478b49e03f772282ce0c6
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-16 17:31:39 +0100

    Add JUnit tests for LevelManager (initial structure)

---
+++ src/testcases/LevelManagerTest.java
@@ -0,0 +1,25 @@
+//Nahal Kianpour Lirharani
+package testcases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LevelManagerTest {
+
+	@Test
+	void testLevelManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCurrentLevel() {
+		fail("Not yet implemented");
+	}
+
+}
