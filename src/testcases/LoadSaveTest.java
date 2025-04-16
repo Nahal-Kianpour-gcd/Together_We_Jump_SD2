612fc68a12fa993460bd1dd7b3d66e33d8ef8e96
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-16 17:17:54 +0100

    Add skeleton JUnit test class for LoadSave methods

---
+++ src/testcases/LoadSaveTest.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoadSaveTest {
+
+	@Test
+	void testGetCharacterSprite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLevelSprite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLevelData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLevelSpriteString() {
+		fail("Not yet implemented");
+	}
+
+}
