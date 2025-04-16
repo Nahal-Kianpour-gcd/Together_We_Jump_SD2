259ecaa1d109dc6cf1dbf02504a1bcddd9596c7e
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-16 17:58:18 +0100

    Add test for calculateUPS with faster timing

--- src/testcases/GameTest.java
+++ src/testcases/GameTest.java
@@ -13,4 +13,16 @@ class GameTest {
 		int ups = Game.calculateUPS(200, 1000);
 		assertEquals(200, ups, "Expected 200 UPS when 200 updates happen in 1 second.");
 	}
+	
+	@Test
+	void testCalculateUPSFastTime() {
+	    // Given: 100 updates in 500ms should mean 200 UPS
+	    int updates = 100;
+	    long timeMillis = 500;
+
+	    int result = Game.calculateUPS(updates, timeMillis);
+
+	    assertEquals(200, result, "UPS should scale correctly with faster update timing.");
+	}
+
 }
