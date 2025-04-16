dec4ea87dddeecbc867d890345446adef7c7d4f1
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-16 17:55:19 +0100

    Add calculateUPS method to Game and corresponding unit test

---
+++ src/testcases/GameTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	@Test
+	void testCalculateUPS() {
+		fail("Not yet implemented");
+	}
+
+}
