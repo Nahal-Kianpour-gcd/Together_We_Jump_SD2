3c1fd60a4d5486afa66d234e115eed47948dfc27
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-16 17:42:29 +0100

    Add test for getCurrentLevel() in LevelManagerTest

--- src/testcases/LevelManagerTest.java
+++ src/testcases/LevelManagerTest.java
@@ -14,14 +14,16 @@ class LevelManagerTest {
 	@Test
 	void testLevelManager() {
 		// Test that the LevelManager can be instantiated with a valid Game object
-		Game dummyGame = new Game(); // I used a dummy Game instance here; if the constructor launches the UI, I might need to mock it later
+		Game dummyGame = new Game(); // I used a dummy Game instance here; if the constructor launches the UI, I
+										// might need to mock it later
 		LevelManager manager = new LevelManager(dummyGame);
 		assertNotNull(manager, "LevelManager should be created and not null.");
 	}
-	
+
 	@Test
 	void testUpdate() {
-		// Ensure that the update method doesn't throw exceptions (currently empty method)
+		// Ensure that the update method doesn't throw exceptions (currently empty
+		// method)
 		Game dummyGame = new Game();
 		LevelManager manager = new LevelManager(dummyGame);
 		assertDoesNotThrow(() -> manager.update(), "Update method should not throw exceptions.");
@@ -29,7 +31,11 @@ class LevelManagerTest {
 
 	@Test
 	void testGetCurrentLevel() {
-		fail("Not yet implemented");
+		// Test that getCurrentLevel returns a valid level object
+		Game dummyGame = new Game();
+		LevelManager manager = new LevelManager(dummyGame);
+		Level currentLevel = manager.getCurrentLevel();
+		assertNotNull(currentLevel, "Current level should not be null.");
 	}
 
 }
