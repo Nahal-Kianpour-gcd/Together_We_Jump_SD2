a51ae9c388c33fd88b60a6555a138942ffd364c5
Polina Shtefan <polina.shtefan@student.griffith.ie>
2025-04-18 20:21:06 +0100

    Implement testGetSpriteAmount_idle() method.

--- src/testcases/AnimationTest.java
+++ src/testcases/AnimationTest.java
@@ -1,8 +1,17 @@
 package testcases;
 
 import org.junit.jupiter.api.Test;
+
+import utilz.Constants;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AnimationTest {
 
+	 @Test
+	    void testGetSpriteAmount_idle() {
+	        int expected = Constants.IDLE_FRAMES;
+	        int actual = Constants.getSpriteAmount(Constants.IDLE_FRAMES);
+	        assertEquals(expected, actual);
+	    }
 }
