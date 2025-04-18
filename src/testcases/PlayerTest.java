fecd09cf59645a9516539d96cb0da8b73e2bb516
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-18 18:00:06 +0100

    Add PlayerTest class with method stubs for Player unit tests

---
+++ src/testcases/PlayerTest.java
@@ -0,0 +1,44 @@
+package testcases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void testInitHitBox() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateHitbox() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLoadLvlData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMoving() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetY() {
+		fail("Not yet implemented");
+	}
+
+}
