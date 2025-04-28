2781e7dd16c4682cf1156e722a98eb3e66b0ae57
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-28 15:44:11 +0100

    add getMinutes() to return total whole minutes left

--- src/timer/Timer.java
+++ src/timer/Timer.java
@@ -10,18 +10,23 @@ public class Timer {
 	public Timer(float seconds) {
 		this.timeRemaining = seconds;
 	}
-	
-    /**
-     * Call once per frame with the elapsed time since last frame.
-     * deltaSeconds time in seconds since last update()
-     */
-    public void update(float deltaSeconds) {
-        // subtract elapsed time, but never go below zero
-        timeRemaining = Math.max(0, timeRemaining - deltaSeconds);
-    }
-    
-    //true once the timer has reached zero //
-    public boolean isFinished() {
-        return timeRemaining <= 0;
-    }
+
+	/**
+	 * Call once per frame with the elapsed time since last frame. deltaSeconds time
+	 * in seconds since last update()
+	 */
+	public void update(float deltaSeconds) {
+		// subtract elapsed time, but never go below zero
+		timeRemaining = Math.max(0, timeRemaining - deltaSeconds);
+	}
+
+	// true once the timer has reached zero //
+	public boolean isFinished() {
+		return timeRemaining <= 0;
+	}
+
+	// total whole minutes left //
+	public int getMinutes() {
+		return (int) (timeRemaining / 60);
+	}
 }
