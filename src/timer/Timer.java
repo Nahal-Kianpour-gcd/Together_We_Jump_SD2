7846b16e8eef14822f574dd2d42ca48ca2e813c6
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-28 15:39:44 +0100

    implement update(deltaSeconds) to subtract elapsed time and floor at zero

--- src/timer/Timer.java
+++ src/timer/Timer.java
@@ -10,4 +10,13 @@ public class Timer {
 	public Timer(float seconds) {
 		this.timeRemaining = seconds;
 	}
+	
+    /**
+     * Call once per frame with the elapsed time since last frame.
+     * @param deltaSeconds time in seconds since last update()
+     */
+    public void update(float deltaSeconds) {
+        // subtract elapsed time, but never go below zero
+        timeRemaining = Math.max(0, timeRemaining - deltaSeconds);
+    }
 }
