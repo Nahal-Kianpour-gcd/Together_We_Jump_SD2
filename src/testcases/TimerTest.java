0c8c64a0db049edc15dc80a7b4f8d6efc11ac8d8
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-29 20:48:47 +0100

    add basic test to verify initial time formatting

--- src/testcases/TimerTest.java
+++ src/testcases/TimerTest.java
@@ -2,14 +2,18 @@
 package testcases;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*; // Import JUnit assertion methods
+import org.junit.jupiter.api.Test; // Import the JUnit @Test annotation
+import timer.Timer; // Import the class we're testing
 
 class TimerTest {
 
+	// Test if the timer starts with the correct formatted time |NK
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testInitialTime() {
+		Timer timer = new Timer(120f); // 120 seconds = 2 minutes
+		assertEquals("02:00", timer.getTimeString()); // Expect formatted time to be 02:00
 	}
 
 }
