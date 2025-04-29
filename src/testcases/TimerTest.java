70c552a421fca3c1342cb125835614fdc9b60dd7
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-04-29 20:46:27 +0100

    add skeleton  JUnit tests for Timer

---
+++ src/testcases/TimerTest.java
@@ -0,0 +1,15 @@
+//Nahal Kianpour Lirharani
+package testcases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TimerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
