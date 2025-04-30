0f0c67800290e574bcf7eec051411f18bed9c802
Polina Shtefan <polina.shtefan@student.griffith.ie>
2025-04-30 08:39:14 +0100

    Refactor addTestCoins() method to work according to the level design.

--- src/entities/CoinManager.java
+++ src/entities/CoinManager.java
@@ -3,6 +3,7 @@ package entities;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CoinManager {
 	private List<Coin> coins;
@@ -15,9 +16,26 @@ public class CoinManager {
     }
     
     public void addTestCoins() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 3; j++) {
-                coins.add(new Coin(100 + i * 100, 100 + j * 100, 20, 20));
+        int coinWidth = 15;
+        int coinHeight = 15;
+        int xSpacing = 160;
+        int ySpacing = 120;
+        int baseX = 100;
+        int baseY = 180;
+
+        Random rand = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 2; j++) {
+                if ((i == 3 && j == 0) || (i == 1 && j == 1)) {
+                    continue;
+                }
+
+                int randomOffsetX = rand.nextInt(41) - 20;
+                int randomOffsetY = rand.nextInt(41) - 30;
+                int x = baseX + i * xSpacing + randomOffsetX;
+                int y = baseY + j * ySpacing + randomOffsetY;
+                coins.add(new Coin(x, y, coinWidth, coinHeight));
             }
         }
     }
