6ad1329c8cdcee6b8d4137cf741044ff78c51ab9
Polina Shtefan <polina.shtefan@student.griffith.ie>
2025-04-30 08:54:58 +0100

    Add Strawberry.png to the World folder. Refactor Coin.java class, so now instead of yellow squares (coins) players are collecting strawberries.

--- src/entities/Coin.java
+++ src/entities/Coin.java
@@ -3,10 +3,17 @@ package entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Coin {
 
+	private BufferedImage image;
 	private float x, y;
 	private int width, height;
     private Rectangle hitbox;
@@ -18,10 +25,20 @@ public class Coin {
         this.width = width;
         this.height = height;
         initHitbox();
+        loadImage();
     }
 
     private void initHitbox() {
         hitbox = new Rectangle((int) x, (int) y, width, height);
+    }
+    
+    private void loadImage() {
+        try {
+            // Load the image from a file (make sure to use the correct file path)
+            image = ImageIO.read(new File("src/image-resources/World/Strawberry.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     public void update() {
@@ -35,8 +52,21 @@ public class Coin {
     
     public void render(Graphics g) {
         if (active) {
+        	/*
             g.setColor(Color.YELLOW);
             g.fillRect((int) x, (int) y, width, height);
+            */
+        	
+        	 int subImageX = 0;  // X position for the subimage within the image
+             int subImageY = 0;  // Y position for the subimage within the image
+             int subImageWidth = 32;  // Width of the subimage
+             int subImageHeight = 32;  // Height of the subimage
+             
+          // Get the subimage from the full image
+             BufferedImage subImage = image.getSubimage(subImageX, subImageY, subImageWidth, subImageHeight);
+
+             // Draw the subimage at the location of the object (x, y)
+             g.drawImage(subImage, (int) x, (int) y, null);
         }
     }
 
