c58a78a5c37c96ad56fccbd28ff6710296a19511
Polina Shtefan <polina.shtefan@student.griffith.ie>
2025-04-30 07:56:18 +0100

    Create Coin.java with next atttributes: position(x,y), size and active state.

---
+++ src/entities/Coin.java
@@ -0,0 +1,24 @@
+//PS
+package entities;
+
+import java.awt.Rectangle;
+
+public class Coin {
+
+	private float x, y;
+	private int width, height;
+    private Rectangle hitbox;
+    private boolean active = true;
+
+    public Coin(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        initHitbox();
+    }
+
+    private void initHitbox() {
+        hitbox = new Rectangle((int) x, (int) y, width, height);
+    }
+}
