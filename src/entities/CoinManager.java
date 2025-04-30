364f9101155b6c1d019815e527125691c95ab366
Polina Shtefan <polina.shtefan@student.griffith.ie>
2025-04-30 13:25:45 +0100

    Add comments to CoinManager.java.

--- src/entities/CoinManager.java
+++ src/entities/CoinManager.java
@@ -1,4 +1,6 @@
+//POLINA SHTEFAN 
 package entities;
+
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,85 +8,89 @@ import java.util.Random;
 import java.awt.Color;
 import griffith.Game;
 
-
 public class CoinManager {
 	private List<Coin> coins;
-    private Player player1, player2;
+	private Player player1, player2;
 
-    public CoinManager(Player player1, Player player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-        this.coins = new ArrayList<>();
-    }
-    
-    public void addTestCoins() {
-        int coinWidth = 15;
-        int coinHeight = 15;
-        int xSpacing = 160;
-        int ySpacing = 120;
-        int baseX = 100;
-        int baseY = 180;
+	//Constructor.
+	public CoinManager(Player player1, Player player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+		this.coins = new ArrayList<>();
+	}
 
-        Random rand = new Random();
+	//Method to add coins as grid with slight randomness within reach of players.
+	public void addTestCoins() {
+		int coinWidth = 15;
+		int coinHeight = 15;
+		int xSpacing = 160;
+		int ySpacing = 120;
+		int baseX = 100;
+		int baseY = 180;
 
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 2; j++) {
-                if ((i == 3 && j == 0) || (i == 1 && j == 1)) {
-                    continue;
-                }
+		Random rand = new Random();
 
-                int randomOffsetX = rand.nextInt(41) - 20;
-                int randomOffsetY = rand.nextInt(41) - 30;
-                int x = baseX + i * xSpacing + randomOffsetX;
-                int y = baseY + j * ySpacing + randomOffsetY;
-             // Only add coin if it’s within screen bounds |NK
-                if (x > 0 && x < Game.GAME_WIDTH - 32 && y > 0 && y < Game.GAME_HEIGHT - 32) {
-                    coins.add(new Coin(x, y, coinWidth, coinHeight));
-                    System.out.println("coin added at: x=" + x + ", y=" + y);
-                }
-                /* OLD CODE:
-                coins.add(new Coin(x, y, coinWidth, coinHeight));
-                */
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 2; j++) {
+				if ((i == 3 && j == 0) || (i == 1 && j == 1)) {
+					continue;
+				}
 
-            }
-        }
-    }
-    
-    public void update() {
-        for (Coin coin : coins) {
-            if (coin.isActive()) {
-                coin.update();
-                checkCollision(coin);
-            }
-        }
-    }
-    
-    private void checkCollision(Coin coin) {
-        if (coin.getHitbox().intersects(player1.getHitbox())) {
-            coin.setActive(false);
-        }
-        if (coin.getHitbox().intersects(player2.getHitbox())) {
-            coin.setActive(false);
-        }
-    }
-    
-    public void render(Graphics g) {
-        for (Coin coin : coins) {
-        	coin.render(g);
-        }
+				int randomOffsetX = rand.nextInt(41) - 20;
+				int randomOffsetY = rand.nextInt(41) - 30;
+				int x = baseX + i * xSpacing + randomOffsetX;
+				int y = baseY + j * ySpacing + randomOffsetY;
+				// Only add coin if it’s within screen bounds |NK
+				if (x > 0 && x < Game.GAME_WIDTH - 32 && y > 0 && y < Game.GAME_HEIGHT - 32) {
+					coins.add(new Coin(x, y, coinWidth, coinHeight));
+					System.out.println("coin added at: x=" + x + ", y=" + y);
+				}
+				/*
+				 * OLD CODE: coins.add(new Coin(x, y, coinWidth, coinHeight));
+				 */
 
-    }
- // Checks if all coins in the game have been collected (none are active). 
- // Returns true only if all coins are inactive (collected). |NK
- public boolean allCoinsCollected() {
-     int remaining = 0;
-     for (Coin coin : coins) {
-         if (coin.isActive()) {
-             remaining++;
-         }
-     }
+			}
+		}
+	}
 
-     return remaining == 0;
- }
+	//Method to update active coin and check collisions.
+	public void update() {
+		for (Coin coin : coins) {
+			if (coin.isActive()) {
+				coin.update();
+				checkCollision(coin);
+			}
+		}
+	}
 
+	//Method to set coin as not active if player collides with it.
+	private void checkCollision(Coin coin) {
+		if (coin.getHitbox().intersects(player1.getHitbox())) {
+			coin.setActive(false);
+		}
+		if (coin.getHitbox().intersects(player2.getHitbox())) {
+			coin.setActive(false);
+		}
+	}
+
+	//Method to render all coins on the screen.
+	public void render(Graphics g) {
+		for (Coin coin : coins) {
+			coin.render(g);
+		}
+
+	}
+
+	// Checks if all coins in the game have been collected (none are active).
+	// Returns true only if all coins are inactive (collected). |NK
+	public boolean allCoinsCollected() {
+		int remaining = 0;
+		for (Coin coin : coins) {
+			if (coin.isActive()) {
+				remaining++;
+			}
+		}
+
+		return remaining == 0;
+	}
 }
